8d1d348266ac6d70d676433a2904e86165d0f510
Gautier M <129167982+GautierMrc@users.noreply.github.com>
2024-05-14 15:18:04 +0100

    responsive
    responsive ... mais il faut reviori le degénage

---
+++ script/burgeur.js
@@ -0,0 +1,7 @@
+var menu = document.querySelector('img[alt="responsiv logo"]');
+var nav = document.querySelector(".botom-header");
+
+menu.addEventListener("click", function() {
+    nav.style.display = (nav.style.display === 'none' || nav.style.display === '') ? 'flex' : 'none';
+    nav.style.flexDirection = 'column'; // Ajoutez cette ligne pour afficher la navigation verticalement
+});
