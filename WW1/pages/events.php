6f050aa754d196bec0088967e187553964c3b960
Kadidya <168824745+Kadidya@users.noreply.github.com>
2024-05-15 14:50:24 +0100

    Add files via upload

---
+++ WW1/pages/events.php
@@ -0,0 +1,23 @@
+<div id='events'>
+    <h1>EVENTS</h1>
+        <div class="eventsone">
+            <article><img src="images/eventimage.png" alt="events"></article>
+            <article>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus accusamus aut labore? Nostrum doloremque magnam expedita aliquam id iste eligendi distinctio, tenetur est corporis odio nemo? Aliquam repudiandae officia voluptatem?</p>
+            </article>
+        </div>
+
+        <div class="eventstwo">
+            <article>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit a quisquam quos at fugiat possimus autem iusto ullam consequuntur in, omnis non velit, soluta fuga ipsum, quia dignissimos. Aliquid, cum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae natus eligendi harum laudantium deleniti ratione distinctio! Animi molestiae quos dignissimos at quidem voluptatum doloribus odit? Nisi cum deserunt iste animi.</p>
+            </article>
+            <article><img src="images/soldat.png" alt="soldat"></article>
+        </div>
+        
+        <div class="eventsthree">
+            <article><img src="images/film.png" alt="movie"></article>
+            <article>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam nisi voluptatem voluptatum exercitationem consequuntur, quae minima nobis, dignissimos adipisci aspernatur rerum? Temporibus harum porro amet ut animi hic doloremque tempora.</p>
+            </article>
+        </div> 
+    </div>
