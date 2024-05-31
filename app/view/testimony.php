7a8ea23ff719703c572441850e0b1386582704de
Gautier M <129167982+GautierMrc@users.noreply.github.com>
2024-05-31 13:21:02 +0100

    ajout text testimoni

--- app/view/testimony.php
+++ app/view/testimony.php
@@ -6,14 +6,14 @@
         </article>
         <article class="testimony-text"> 
             <h2>Name</h2>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis esse quod voluptatem modi a. Consectetur laborum molestias et eaque libero atque amet fugit dicta, error labore, iure repellat facilis facere?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos adipisci consequuntur corrupti alias accusamus unde qui esse assumenda repudiandae, eligendi cumque quis sint perspiciatis nostrum, iure placeat voluptatem ex Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis quasi molestias, consectetur, sint voluptas omnis assumenda illo nam facere voluptatem temporibus. Ipsum odio dolorem earum aliquam, aut iusto natus consectetur.</p>
+            <p> I discovered World War I through my mom, who taught me the story of my family and how they fought for England. At that time, we didn't learn this story at school. The more I listened to my mother's stories, the more I fell in love with that historical period. Since that day, I made up my mind to preserve and honor our historical heritage.consequuntur corrupti alias accusamus unde qui esse assumenda repudiandae, eligendi cumque quis sint perspiciatis nostrum, iure placeat voluptatem ex Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis quasi molestias, consectetur, sint voluptas omnis assumenda illo nam facere voluptatem temporibus. Ipsum odio dolorem earum aliquam, aut iusto natus consectetur.</p>
         </article>
     </div>
 
     <div class="testimony">
         <article class="testimony-text">
             <h2>Name</h2>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid, harum? Iusto quisquam amet ipsa numquam quaerat fugit vitae quos eius debitis totam. Enim reiciendis corporis veniam, atque error accusantium Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab quidem fuga cupiditate rerum iure dolor, id earum mollitia aspernatur quae aut nesciunt inventore vel quos totam voluptates, commodi soluta modi?Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam corrupti iure vitae temporibus aspernatur distinctio nostrum ex aut, ducimus optio assumenda eligendi quo voluptate dolorem error iste magnam reprehenderit deserunt!</p>
+            <p>One day, my friend and I decided to visit "La Somme" in France. When we arrived, we saw many skeletons and buried trenches. Indeed, at that time, this place wasn't as clean as it is today. We truly saw the sorrow of war. After this exploration, we went for a walk, hoping to meet people to talk with and hear their testimonies about the famous battles they had experienced. At this moment, we met a couple of English people (which was really rare at that time). After a brief exchange, the woman said to us, "If you want more information, don't hesitate to knock on doors and ask questions you have.</p>
         </article>
         <article class="testimony-img">
             <img src="/assets/img/testimony/testimony2.png" alt="photo">
@@ -26,7 +26,7 @@
         </article>
         <article class="testimony-text">
             <h2>Name</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem totam praesentium, accusamus dignissimos, repellat id minima provident iste ab maxime molestiae reiciendis tempore consequatur voluptas mollitia sed fugit dolor. Quaerat. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum alias, cumque voluptate natus, ut tenetur possimus porro pariatur ab fuga, rem rerum libero cum? Delectus eos placeat debitis animi quis?Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, repudiandae sed, a voluptatem laborum dolore eum ad commodi tempore necessitatibus autem iste omnis distinctio sint laboriosam cumque accusamus voluptatum facilis!</p>
+            <p>I remember answering her, "No, I can't," but she retorted, "Shut up and go." So that's what we decided to do. Arriving at the first farm, we knocked on the door. The inhabitant was so welcoming that we had a magnificent time with him. Since that day, we became such good friends that I met all his family and even attended his friend's wedding. I will always be grateful for what this Frenchman gave me, the way he explained the history as if I were his own son.</p>
         </article>
     </div>
 </section>
