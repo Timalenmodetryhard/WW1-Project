b6d15fea762c30e55e87e12e59a59a4e2bbf1382
Gautier M <129167982+GautierMrc@users.noreply.github.com>
2024-05-31 13:32:04 +0100

    encore du texte

--- app/view/testimony.php
+++ app/view/testimony.php
@@ -5,14 +5,14 @@
             <img src="/assets/img/testimony/testimony1.png" alt="photo">
         </article>
         <article class="testimony-text"> 
-            <h2>Name</h2>
-            <p> I discovered World War I through my mom, who taught me the story of my family and how they fought for England. At that time, we didn't learn this story at school. The more I listened to my mother's stories, the more I fell in love with that historical period. Since that day, I made up my mind to preserve and honor our historical heritage.consequuntur corrupti alias accusamus unde qui esse assumenda repudiandae, eligendi cumque quis sint perspiciatis nostrum, iure placeat voluptatem ex Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis quasi molestias, consectetur, sint voluptas omnis assumenda illo nam facere voluptatem temporibus. Ipsum odio dolorem earum aliquam, aut iusto natus consectetur.</p>
+            <h2>Pation for WW1</h2>
+            <p> I discovered World War I through my mom, who taught me the story of my family and how they fought for England. At that time, we didn't learn this story at school. The more I listened to my mother's stories, the more I fell in love with that historical period. Since that day, I made up my mind to preserve and honor our historical heritage.</p>
         </article>
     </div>
 
     <div class="testimony">
         <article class="testimony-text">
-            <h2>Name</h2>
+            <h2>First time at 'La Somme'</h2>
             <p>One day, my friend and I decided to visit "La Somme" in France. When we arrived, we saw many skeletons and buried trenches. Indeed, at that time, this place wasn't as clean as it is today. We truly saw the sorrow of war. After this exploration, we went for a walk, hoping to meet people to talk with and hear their testimonies about the famous battles they had experienced. At this moment, we met a couple of English people (which was really rare at that time). After a brief exchange, the woman said to us, "If you want more information, don't hesitate to knock on doors and ask questions you have.</p>
         </article>
         <article class="testimony-img">
@@ -25,7 +25,7 @@
             <img src="/assets/img/testimony/testimony3.png" alt="photo">
         </article>
         <article class="testimony-text">
-            <h2>Name</h2>
+            <h2>When I met a frendly frensh farmer</h2>
             <p>I remember answering her, "No, I can't," but she retorted, "Shut up and go." So that's what we decided to do. Arriving at the first farm, we knocked on the door. The inhabitant was so welcoming that we had a magnificent time with him. Since that day, we became such good friends that I met all his family and even attended his friend's wedding. I will always be grateful for what this Frenchman gave me, the way he explained the history as if I were his own son.</p>
         </article>
     </div>
