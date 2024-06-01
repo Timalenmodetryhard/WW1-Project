5b55a06eb1c131e522f60d6b7c82d11598e617db
Gautier M <129167982+GautierMrc@users.noreply.github.com>
2024-06-01 10:24:00 +0100

    ajout script changement langue
    Ajout du script de changement de langue...qui pour l'insant ne fonctionne pas  :{

--- app/view/footer.php
+++ app/view/footer.php
@@ -1,3 +1,4 @@
+  <script src='/assets/script/langue.js' type='text/javascript'></script>
   <footer>
     <div class="footerup">
       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2519.967896594537!2d-1.0554378224226981!3d50.8317585598346!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48745db817fda771%3A0xecdbe5a7a0e72152!2sThe%20WW1%20Remembrance%20Centre!5e0!3m2!1sfr!2suk!4v1715770722853!5m2!1sfr!2suk" class="map" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
